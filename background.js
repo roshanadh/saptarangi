000d6e3435da2cf8a594216ef5d396658d5e2d6b
Roshan Adhikari <nahsorad@gmail.com>
2019-04-07 12:14:58 +0545

    Implement onClick event handler

--- background.js
+++ background.js
@@ -1,4 +1,12 @@
 chrome.browserAction.onclicked.addListener((tab) => {
 	const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+	
+	const colorPicker = () => {
+		const randomIndex = Math.floow(Math.random() * colors.length);
+		return colors[randomIndex];
+	}
 
-})
+	chrome.tabs.executeScript({
+		code: 'document.body.style.backgroundColor = "' + colorPicker() + '"'
+	});
+});
